1d5522079fa8b363f4ef7b7442192dad076ce485
Marco Mignacco <marcomignacco03@gmail.com>
2024-11-17 18:06:14 -0300

    first commit

---
+++ src/main/java/com/example/TpTaller/TpTallerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TpTaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpTallerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TpTaller/TpTallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TpTaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpTallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
